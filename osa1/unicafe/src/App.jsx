cf60f38e63967b44f571a3779365e41fba2ba72b
katja <katja.kumpulainen03@gmail.com>
2024-07-04 13:39:25 +0300

    1.7 unicafe tehty

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,22 +1,47 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        all {props.total} <br/>
+        average {props.average} <br />
+        positive {props.positiveFeedback} %
+      </p>
+    </div>
+  );
+};
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const handleGoodClick = () => {
-    setGood(good + 1);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    setTotal(updatedGood + neutral + bad);
   };
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    setTotal(updatedNeutral + good + bad);
   };
 
   const handleBadClick = () => {
-    setBad(bad + 1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setTotal(updatedBad + good + neutral);
   };
+
+  const average = total === 0 ? 0 : (good - bad) / total;
+
+  const positiveFeedback =
+    total === 0 ? 0 : (good / (good + bad + neutral)) * 100;
+ 
 
   return (
     <div>
@@ -27,10 +52,12 @@ const App = () => {
         <button onClick={handleBadClick}>bad</button>
         <h2>statistics</h2>
         <p>
-          good {good} <br/>
-          neutral {neutral} <br/>
-          bad {bad}
+        good {good} <br />
+        neutral {neutral} <br/>
+        bad {bad} <br />
         </p>
+        <Statistics total={total} average={average.toFixed(3)} positiveFeedback={positiveFeedback.toFixed(3)} />
+
       </div>
     </div>
   );

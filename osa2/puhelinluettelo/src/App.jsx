8e47ca4c4b19493aab6a17bd16f96a0f22fe9297
katja <katja.kumpulainen03@gmail.com>
2024-08-21 19:40:53 +0300

    2.6. puhelinluettelo tehty

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './app.css';
+
+const App = (props) => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {name: newName}
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style={{ listStyle: 'none' }}>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
